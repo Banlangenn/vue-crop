e9319aa52ca5a785ea244d49d17042010f95f67e
Banlangen <928012047@qq.com>
2019-03-28 15:39:32 +0800

    Update vue.config.js
    update

--- vue.config.js
+++ vue.config.js
@@ -34,7 +34,7 @@ module.exports = {
             .use('babel')
             .loader('babel-loader')
             .tap(options => {
-                // 修改它的选项...
+                // 修改它的选项==>
                 return options;
             });
     }
