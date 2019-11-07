bce78646671d160906f36cb1c390aeddfa8ae517
ljw <928012047@qq.com>
2019-11-07 16:35:23 +0800

    加入seve

---
+++ socketServe.js
@@ -0,0 +1,85 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+ 
+app.get('/', function(req, res){
+    res.send('<h1>Welcome Realtime Server</h1>');
+});
+ 
+//在线用户
+var onlineUsers = {};
+//当前在线人数
+var onlineCount = 0;
+let groupOwnerId = null
+io.on('connection', function(socket){
+    console.log('a user connected');
+     
+    //监听新用户加入
+    socket.on('login', function(obj){
+        //将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
+        socket.name = obj.userid;
+        const ID = socket.id
+         
+        //检查在线列表，如果不在里面就加入
+        if(!onlineUsers.hasOwnProperty(obj.userid)) {
+            onlineUsers[obj.userid] = obj.username;
+            //在线人数+1
+            onlineCount++;
+        }
+        if (obj.type == 2) {
+            groupOwnerId = ID
+        } else {
+            // 学生向老师 发送自己的id  老师socket
+            console.log('学生向老师 发送自己的id  老师socket')
+            // const toSocket = io.sockets.sockets[groupOwnerId]
+            if (groupOwnerId &&  io.sockets.sockets[groupOwnerId]) {
+                io.sockets.sockets[groupOwnerId].emit('toOne', {id: socket.id})
+            }
+
+        }
+         
+        // 向所有客户端广播用户加入
+        io.emit('login', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
+        
+        console.log(obj.username+'加入了聊天室');
+    });
+
+    // 老师 向学生发送自己的 当前数据状态
+    socket.on('toOne',function(msgObj){
+        // 找到学生的 socket
+        console.log('有新学生上线---给学生发送自己状态')
+        const { id, ...rest } = msgObj
+        const toSocket = io.sockets.sockets[id]
+        toSocket.emit('toOne', rest);
+    })
+    //监听用户退出
+    socket.on('disconnect', function(){
+        //将退出的用户从在线列表中删除
+        if(onlineUsers.hasOwnProperty(socket.name)) {
+            //退出用户的信息
+            var obj = {userid:socket.name, username:onlineUsers[socket.name]};
+             
+            //删除
+            delete onlineUsers[socket.name];
+            //在线人数-1
+            onlineCount--;
+             
+            //向所有客户端广播用户退出
+            io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
+            console.log(obj.username+'退出了聊天室');
+        }
+    });
+     
+    //监听用户发布聊天内容
+    socket.on('message', function(obj){
+        //向所有客户端广播发布的消息
+        // io.emit('message', obj);
+        console.log('老师发数据了 我要广播了')
+        socket.broadcast.emit('message', obj);
+    });
+   
+});
+ 
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
